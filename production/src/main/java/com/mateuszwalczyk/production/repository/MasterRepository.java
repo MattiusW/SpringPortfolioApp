b603d9603e4bdd7fde22182240dce1389f67cfa2
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-21 11:22:29 +0100

    Create master repository

---
+++ production/src/main/java/com/mateuszwalczyk/production/repository/MasterRepository.java
@@ -0,0 +1,11 @@
+package com.mateuszwalczyk.production.repository;
+
+import com.mateuszwalczyk.production.model.Master;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.rowset.CachedRowSet;
+
+@Repository
+public interface MasterRepository extends JpaRepository<Master,Long> {
+}
