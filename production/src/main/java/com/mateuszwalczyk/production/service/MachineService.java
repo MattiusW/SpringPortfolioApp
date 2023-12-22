c93a0f1dce547f31e92b2985a8c0bfcc039b94e2
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-22 09:55:03 +0100

    Create MachineService

---
+++ production/src/main/java/com/mateuszwalczyk/production/service/MachineService.java
@@ -0,0 +1,18 @@
+package com.mateuszwalczyk.production.service;
+
+import com.mateuszwalczyk.production.model.Machine;
+import com.mateuszwalczyk.production.repository.MachineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MachineService {
+
+    @Autowired
+    MachineRepository machineRepository;
+
+    public Machine saveMachine(Machine machine){
+        return machineRepository.save(machine);
+    }
+
+}
