45198455c7b266f87bc0fdcfdf6dd925044847fa
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-22 10:11:02 +0100

    PostMapping

--- production/src/main/java/com/mateuszwalczyk/production/controller/WorkerController.java
+++ production/src/main/java/com/mateuszwalczyk/production/controller/WorkerController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/worker")
 public class WorkerController {
 
     @Autowired
