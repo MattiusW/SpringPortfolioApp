dacfb0c9bb72073d57a26aedcb61c90d13521db1
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-22 09:54:52 +0100

    Create MachineController

---
+++ production/src/main/java/com/mateuszwalczyk/production/controller/MachineController.java
@@ -0,0 +1,21 @@
+package com.mateuszwalczyk.production.controller;
+
+import com.mateuszwalczyk.production.model.Machine;
+import com.mateuszwalczyk.production.service.MachineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class MachineController {
+
+    @Autowired
+    MachineService machineService;
+
+    @PostMapping
+    public Machine createMachine(@RequestBody Machine machine){
+        return machineService.saveMachine(machine);
+    }
+
+}
