89176d56a75fcec45f179a988c7df6ef727619ef
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-22 09:24:38 +0100

    Extends CrudRepository

--- production/src/main/java/com/mateuszwalczyk/production/repository/MasterRepository.java
+++ production/src/main/java/com/mateuszwalczyk/production/repository/MasterRepository.java
@@ -2,10 +2,11 @@ package com.mateuszwalczyk.production.repository;
 
 import com.mateuszwalczyk.production.model.Master;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.rowset.CachedRowSet;
 
 @Repository
-public interface MasterRepository extends JpaRepository<Master,Long> {
+public interface MasterRepository extends CrudRepository<Master,Long> {
 }
