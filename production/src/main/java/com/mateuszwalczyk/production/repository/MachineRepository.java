c88ce6edfa905f71903d98bb2dc0bd71a9d891a0
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-24 11:40:12 +0100

    Add view all machine

--- production/src/main/java/com/mateuszwalczyk/production/repository/MachineRepository.java
+++ production/src/main/java/com/mateuszwalczyk/production/repository/MachineRepository.java
@@ -1,9 +1,16 @@
 package com.mateuszwalczyk.production.repository;
 
 import com.mateuszwalczyk.production.model.Machine;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MachineRepository extends CrudRepository<Machine, Long> {
+
+    @Query("SELECT i FROM Machine i")
+    public List<Machine> getAllMachine();
+
 }
--- production/src/main/java/com/mateuszwalczyk/production/service/MachineService.java
+++ production/src/main/java/com/mateuszwalczyk/production/service/MachineService.java
@@ -5,6 +5,8 @@ import com.mateuszwalczyk.production.repository.MachineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MachineService {
 
@@ -15,4 +17,7 @@ public class MachineService {
         return machineRepository.save(machine);
     }
 
+    public List<Machine> getAllMachine(){
+        return machineRepository.getAllMachine();
+    }
 }
