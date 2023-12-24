6e1efe77893f30fd4efeff0281e3fbbc28bbc878
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-24 12:51:27 +0100

    Test all worker view

--- production/src/test/java/com/mateuszwalczyk/production/controller/WorkerControllerTest.java
+++ production/src/test/java/com/mateuszwalczyk/production/controller/WorkerControllerTest.java
@@ -10,11 +10,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.awt.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -48,6 +49,20 @@ public class WorkerControllerTest {
 
         // Check if status is 200
         result.andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void testGetAllWorkers() throws Exception{
+
+        //Get method
+        ResultActions result = mockMvc.perform(get("/worker/all")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //Check is status 200 and anserw JSON is list worker
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+
     }
 
 }
